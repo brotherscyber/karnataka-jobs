e15780b47af59f6ecf0fa813c90601faf075bb0e
King <king@Kings-iMac.local>
2026-06-15 13:42:51 +0530

    Dynamic sitemap for jobs

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,8 +1,20 @@
-export default function sitemap() {
+import { db } from "../lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+export default async function sitemap() {
+  const snapshot = await getDocs(collection(db, "jobs"));
+
+  const jobs = snapshot.docs.map((doc) => ({
+    url: `https://karnataka-jobs.vercel.app/jobs/${doc.id}`,
+    lastModified: new Date(),
+  }));
+
   return [
     {
       url: "https://karnataka-jobs.vercel.app",
       lastModified: new Date(),
     },
+
+    ...jobs,
   ];
 }
