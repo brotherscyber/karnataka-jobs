422428f2326cc30ee2ce50aaf6404b8e858ce4e2
King <king@Kings-iMac.local>
2026-06-15 13:45:02 +0530

    Restore sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,20 +1,8 @@
-import { db } from "../lib/firebase";
-import { collection, getDocs } from "firebase/firestore";
-
-export default async function sitemap() {
-  const snapshot = await getDocs(collection(db, "jobs"));
-
-  const jobs = snapshot.docs.map((doc) => ({
-    url: `https://karnataka-jobs.vercel.app/jobs/${doc.id}`,
-    lastModified: new Date(),
-  }));
-
+export default function sitemap() {
   return [
     {
       url: "https://karnataka-jobs.vercel.app",
       lastModified: new Date(),
     },
-
-    ...jobs,
   ];
 }
